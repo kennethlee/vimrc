1f7fbf6626fb9a0aaae6eb8ea3501320caf6a446
Kenneth Lee <kennyxlee@gmail.com>
2021-06-19 22:04:57 -0700

    Create toggle for LSP virtual text

--- .config/nvim/lua/lsp/custom.lua
+++ .config/nvim/lua/lsp/custom.lua
@@ -20,12 +20,26 @@ function M.on_init()
 end
 
 function M.lsp_keymap()
-  vim.api.nvim_buf_set_keymap(0, "n", "gn", "<cmd>lua vim.lsp.diagnostic.goto_next({enable_popup = true})<CR>", {noremap = true, silent = true})
-  vim.api.nvim_buf_set_keymap(0, "n", "gp", "<cmd>lua vim.lsp.diagnostic.goto_prev({enable_popup = true})<CR>", {noremap = true, silent = true})
-  vim.api.nvim_buf_set_keymap(0, "n", "<Space>gl", "<cmd>lua vim.lsp.diagnostic.set_loclist({open_loclist = true})<CR>", {noremap = true, silent = true})
-  vim.api.nvim_buf_set_keymap(0, "n", "<Space>gf", "<cmd>lua vim.lsp.buf.formatting()<CR>", {noremap = true, silent = true})
+  virtual_text = {}
+  virtual_text.show = true
+
+  virtual_text.toggle = function()
+    virtual_text.show = not virtual_text.show
+    vim.lsp.diagnostic.display(
+      vim.lsp.diagnostic.get(0, 1),
+      0,
+      1,
+      {virtual_text = virtual_text.show}
+    )
+  end
+
+  vim.api.nvim_buf_set_keymap(0, "n", "gn", "<cmd>lua vim.lsp.diagnostic.goto_next({enable_popup = false})<CR>", {noremap = true, silent = true})
+  vim.api.nvim_buf_set_keymap(0, "n", "gp", "<cmd>lua vim.lsp.diagnostic.goto_prev({enable_popup = false})<CR>", {noremap = true, silent = true})
   vim.api.nvim_buf_set_keymap(0, "n", "<Space>gd", "<cmd>lua vim.lsp.buf.definition()<CR>", {noremap = true, silent = true})
+  vim.api.nvim_buf_set_keymap(0, "n", "<Space>gf", "<cmd>lua vim.lsp.buf.formatting()<CR>", {noremap = true, silent = true})
   vim.api.nvim_buf_set_keymap(0, "n", "<Space>gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", {noremap = true, silent = true})
+  vim.api.nvim_buf_set_keymap(0, "n", "<Space>gj", "<cmd>lua virtual_text.toggle()<CR>", {silent=true, noremap=true})
+  vim.api.nvim_buf_set_keymap(0, "n", "<Space>gl", "<cmd>lua vim.lsp.diagnostic.set_loclist({open_loclist = true})<CR>", {noremap = true, silent = true})
   vim.api.nvim_buf_set_keymap(0, "n", "<Space>gr", "<cmd>lua vim.lsp.buf.references()<CR>", {noremap = true, silent = true})
 end
 
