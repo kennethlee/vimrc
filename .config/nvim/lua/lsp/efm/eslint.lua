399a68d2696bb7bce72caf04e9c6a1f6a6b65e1c
Ken Lee <ken.lee.dev@gmail.com>
2023-03-22 17:17:25 -0700

    Remove config for efm-langserver

--- .config/nvim/lua/lsp/efm/eslint.lua
+++
@@ -1,8 +0,0 @@
-return {
-  lintCommand = "./node_modules/.bin/eslint -f unix --stdin",
-  lintStdin = true,
-  lintFormats = {"%f:%l:%c: %m"},
-  lintIgnoreExitCode = true,
-  formatCommand = "eslint --fix-to-stdout --stdin --stdin-filename=${INPUT}",
-  formatStdin = true,
-}
--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -1,2 +1,1 @@
-require("lsp/efm")
 require("lsp/ts")
