eb99d14c7fec7af047855c0dfaed38006486d916
Ken Lee <ken.lee.dev@gmail.com>
2023-03-22 17:07:39 -0700

    Fix: broken LSP-related keymappings

--- .config/nvim/lua/lsp/custom.lua
+++ .config/nvim/lua/lsp/custom.lua
@@ -9,7 +9,6 @@ function M.on_init()
       virtual_text = false,
     }
   )
-
   print("LSP initialized.")
 end
 
@@ -26,14 +25,14 @@ function M.lsp_keymap()
     end
   end
 
-  vim.api.nvim_buf_set_keymap(0, "n", "gn", "<cmd>lua vim.lsp.diagnostic.goto_next({enable_popup = false})<CR>", {noremap = true, silent = true})
-  vim.api.nvim_buf_set_keymap(0, "n", "gp", "<cmd>lua vim.lsp.diagnostic.goto_prev({enable_popup = false})<CR>", {noremap = true, silent = true})
-  vim.api.nvim_buf_set_keymap(0, "n", "<Space>gd", "<cmd>lua vim.lsp.buf.definition()<CR>", {noremap = true, silent = true})
-  vim.api.nvim_buf_set_keymap(0, "n", "<Space>gf", "<cmd>lua vim.lsp.buf.formatting()<CR>", {noremap = true, silent = true})
-  vim.api.nvim_buf_set_keymap(0, "n", "<Space>gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", {noremap = true, silent = true})
-  vim.api.nvim_buf_set_keymap(0, "n", "<Space>gj", "<cmd>lua virtual_text.toggle()<CR>", {silent=true, noremap=true})
-  vim.api.nvim_buf_set_keymap(0, "n", "<Space>gl", "<cmd>lua vim.lsp.diagnostic.set_loclist({open_loclist = true})<CR>", {noremap = true, silent = true})
-  vim.api.nvim_buf_set_keymap(0, "n", "<Space>gr", "<cmd>lua vim.lsp.buf.references()<CR>", {noremap = true, silent = true})
+  vim.api.nvim_buf_set_keymap(0, "n", "gn",         "<cmd>lua vim.diagnostic.goto_next({enable_popup = false})<CR>", {noremap = true, silent = true})
+  vim.api.nvim_buf_set_keymap(0, "n", "gp",         "<cmd>lua vim.diagnostic.goto_prev({enable_popup = false})<CR>", {noremap = true, silent = true})
+  vim.api.nvim_buf_set_keymap(0, "n", "<Space>gd",  "<cmd>lua vim.lsp.buf.definition()<CR>", {noremap = true, silent = true})
+  vim.api.nvim_buf_set_keymap(0, "n", "<Space>gf",  "<cmd>lua vim.lsp.buf.format()<CR>", {noremap = true, silent = true})
+  vim.api.nvim_buf_set_keymap(0, "n", "<Space>gi",  "<cmd>lua vim.lsp.buf.implementation()<CR>", {noremap = true, silent = true})
+  vim.api.nvim_buf_set_keymap(0, "n", "<Space>gj",  "<cmd>lua virtual_text.toggle()<CR>", {silent=true, noremap=true})
+  vim.api.nvim_buf_set_keymap(0, "n", "<Space>gl",  "<cmd>lua vim.diagnostic.setloclist({open_loclist = true})<CR>", {noremap = true, silent = true})
+  vim.api.nvim_buf_set_keymap(0, "n", "<Space>gr",  "<cmd>lua vim.lsp.buf.references()<CR>", {noremap = true, silent = true})
 end
 
 return M
