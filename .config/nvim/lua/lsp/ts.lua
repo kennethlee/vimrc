562b0cc714db5a130745454dd97527c4cb820232
Ken Lee <ken.lee.dev@gmail.com>
2023-03-23 13:28:54 -0700

    Add all possible config options for tsserver LSP
    For the sake of completeness.

--- .config/nvim/lua/lsp/ts.lua
+++ .config/nvim/lua/lsp/ts.lua
@@ -1,16 +1,19 @@
 local lsp_config = require("lspconfig")
 
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#tsserver
 lsp_config.tsserver.setup({
-  root_dir = vim.loop.cwd,
-  init_options = {
-    documentFormatting = false,
-  },
+  cmd = { "typescript-language-server", "--stdio" },
   filetypes = {
     "javascript",
     "javascriptreact",
     "javascript.jsx",
     "typescript",
-    "typescript.tsx",
     "typescriptreact",
+    "typescript.tsx",
   },
+  init_options = {
+    hostInfo = "neovim",
+  },
+  root_dir = vim.loop.cwd,
+  single_file_support = true,
 })
