fa523a9ebcc88800a1c9cba4a9230066b400dd9f
Ken Lee <ken.lee.dev@gmail.com>
2023-04-01 07:55:54 -0700

    Set tabs to 4 spaces for ledger

--- .config/nvim/after/indent/ledger.lua
+++ .config/nvim/after/indent/ledger.lua
@@ -1,6 +1,6 @@
 local opt_local = vim.opt_local
 
 opt_local.expandtab = true
-opt_local.shiftwidth = 2
-opt_local.softtabstop = 2
-opt_local.tabstop = 2
+opt_local.shiftwidth = 4
+opt_local.softtabstop = 4
+opt_local.tabstop = 4
