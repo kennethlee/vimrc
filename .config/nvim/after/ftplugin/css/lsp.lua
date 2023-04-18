16f8632460548734b28ea529859eb4972e21eef8
Ken Lee <ken.lee.dev@gmail.com>
2023-04-18 13:09:09 -0700

    Add LSP config for CSS diagnostics
    Does not add completion; fix later.

---
+++ .config/nvim/after/ftplugin/css/lsp.lua
@@ -0,0 +1,24 @@
+local bin_name = "vscode-css-language-server"
+local cmd = { bin_name, "--stdio" }
+local filetypes = { "css" }
+
+vim.lsp.start({
+  name = bin_name,
+  cmd = cmd,
+  filetypes = filetypes,
+  settings = {
+    css = { validate = true },
+    scss = { validate = true },
+    less = { validate = true },
+    capabilities = {
+      textDocument = {
+        completion = {
+          completionItem = {
+            snippetSupport = true,
+          },
+        },
+      },
+    },
+  },
+  root_dir = vim.loop.cwd()
+})
