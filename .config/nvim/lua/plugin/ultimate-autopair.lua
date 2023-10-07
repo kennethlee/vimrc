583f1e2493416b847addf4ac1a4620ecb66705ca
Ken Lee <ken.lee.dev@gmail.com>
2023-10-06 22:01:50 -0700

    Switch to ultimate-autopair

---
+++ .config/nvim/lua/plugin/ultimate-autopair.lua
@@ -0,0 +1,13 @@
+return {
+  "altermo/ultimate-autopair.nvim",
+  enabled = true,
+  lazy = true,
+  event = { "InsertEnter", "CmdlineEnter" },
+  opts = {
+    -- extensions = {
+    --   filetype = {
+    --     nft = { "", "markdown", "text" },
+    --   },
+    -- },
+  },
+}
