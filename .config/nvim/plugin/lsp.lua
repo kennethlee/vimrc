b41872a7e2498f9b010040a273bea3a7f5f39345
Ken Lee <ken.lee.dev@gmail.com>
2025-03-28 23:50:47 -0700

    LSP: use basic auto-completion

--- .config/nvim/plugin/lsp.lua
+++ .config/nvim/plugin/lsp.lua
@@ -1,101 +1,19 @@
--- opts / {{{1
+-- New default mappings (LSP-related):
+-- grn       = Normal mode -> vim.lsp.buf.rename()
+-- grr       = Normal mode -> vim.lsp.buf.references()
+-- gri       = Normal mode -> vim.lsp.buf.implementation()
+-- gO        = Normal mode -> vim.lsp.buf.document_symbol() (this is analogous to the gO mappings in help buffers and :Man page buffers to show a "table of contents")
+-- gra       = Normal / Visual modes. -> vim.lsp.buf.code_action()
+-- K         = Normal mode -> vim.lsp.buf.hover()
+-- CTRL-S    = Insert / Select modes. -> vim.lsp.buf.signature_help()
+-- [d and ]d = Move between diagnostics in the current buffer ([D jumps to the first diagnostic, ]D jumps to the last)
 
-local diagnostic_config = {
-  float = {
-    border = "rounded",
-    focusable = false,
-    style = "minimal",
-    header = "",
-    prefix = "",
-  },
-  severity_sort = false,
-  signs = true,
-  underline = true,
-  update_in_insert = false,
-  virtual_text = false,
-}
-vim.diagnostic.config(diagnostic_config)
-
---------------------------------------------------------------------------------
--- ui {{{1
-
-local set_hl = vim.api.nvim_set_hl
-local sign_define = vim.fn.sign_define
-
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-  vim.lsp.handlers.hover, {
-    border = "rounded",
-    title = "hover",
-  }
-)
-set_hl(0, "NormalFloat", { link = "Normal" })
-set_hl(0, "FloatBorder", { link = "Normal" })
-
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
-  vim.lsp.handlers.signature_help, {
-    border = "rounded",
-    focusable = false,
-    close_events = { "CursorMoved", "BufHidden", "InsertCharPre" },
-  }
-)
-set_hl(0, "LspSignatureActiveParameter", { link = "WarningMsg" })
-
--- gutter signs
-sign_define(
-  "DiagnosticSignError",
-  { texthl = "DiagnosticSignError", text = "", numhl = "DiagnosticSignError" }
-)
-sign_define(
-  "DiagnosticSignHint",
-  { texthl = "DiagnosticSignHint", text = "", numhl = "DiagnosticSignHint" }
-)
-sign_define(
-  "DiagnosticSignInfo",
-  { texthl = "DiagnosticSignInfo", text = "", numhl = "DiagnosticSignInfo" }
-)
-sign_define(
-  "DiagnosticSignWarn",
-  { texthl = "DiagnosticSignWarn", text = "", numhl = "DiagnosticSignWarn" }
-)
-
---------------------------------------------------------------------------------
--- keymap {{{1
-
-local diagnostics_active = true
-local toggle_diagnostics = function()
-  diagnostics_active = not diagnostics_active
-  if diagnostics_active then
-    vim.diagnostic.show()
-  else
-    vim.diagnostic.hide()
-  end
-end
-
-local autocmd = vim.api.nvim_create_autocmd
-autocmd("LspAttach", {
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    local bufnr = args.buf
-    local set = vim.keymap.set
-    local key_opts = { buffer = bufnr, noremap = true, silent = true }
-
-    vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
-    vim.bo[bufnr].tagfunc = "v:lua.vim.lsp.tagfunc"
-    vim.bo[bufnr].formatexpr = "v:lua.vim.lsp.formatexpr()"
-
-    set("n", "gn",    "<cmd>lua vim.diagnostic.goto_next({ enable_popup = false })<CR>", key_opts)
-    set("n", "gp",    "<cmd>lua vim.diagnostic.goto_prev({ enable_popup = false })<CR>", key_opts)
-    set("n", "gd",    vim.lsp.buf.definition, key_opts)
-    set("n", "gf",    vim.lsp.buf.format, key_opts)
-    set("n", "gi",    "<cmd>lua vim.lsp.buf.implementation()<CR>", key_opts)
-    set("n", "gj",    toggle_diagnostics, key_opts)
-    set("n", "gl",    "<cmd>lua vim.diagnostic.setloclist({ open_loclist = true })<CR>", key_opts)
-    set("n", "gr",    vim.lsp.buf.references, key_opts)
-    set("i", "<C-l>", vim.lsp.buf.signature_help, key_opts)
-
-    print("LSP server initialized.")
+-- basic LSP-based auto-completion via gpanders
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client:supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+    end
   end,
 })
-
---------------------------------------------------------------------------------
--- }}}1
