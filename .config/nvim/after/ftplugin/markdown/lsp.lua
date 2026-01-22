52980646076cc6323f8a38b40c556b7dafbbb147
Ken Lee <ken.lee.dev@gmail.com>
2026-01-21 22:31:05 -0800

    Fix: harper config

--- .config/nvim/after/ftplugin/markdown/lsp.lua
+++ .config/nvim/after/ftplugin/markdown/lsp.lua
@@ -8,5 +8,17 @@ vim.lsp.start({
   name = bin_name,
   cmd = cmd,
   -- filetypes = filetypes,
+  settings = {
+    [bin_name] = {
+      isolateEnglish = true,
+      linters = {
+        WrongQuotes = true,
+      },
+      markdown = {
+        IgnoreLinkTitle = true,
+      },
+      maxFileLength = 1000000000,
+    }
+  },
   root_dir = vim.fn.getcwd(),
 })
