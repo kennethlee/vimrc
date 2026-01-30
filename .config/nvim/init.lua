afb65f40e406dcb645f249e77857bdf462796f35
Ken Lee <ken.lee.dev@gmail.com>
2026-01-29 22:14:32 -0800

    Switch to default statusline

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -239,19 +239,6 @@ end, key_opts)
 --------------------------------------------------------------------------------
 -- ui {{{1
 
-local stl = {
-  "%#error#",      -- error highlight group begin
-  "%m",            -- modified flag
-  "%*",            -- error highlight group end
-  "%<",            -- truncation point
-  "%f",            -- filepath
-  "%=",            -- separator
-  "%{v:register}", -- display current register
-  "%-18",          -- padding right, 18 char width
-  "(%l,%c%V%)",    -- current {line #},{column #}-{virtual column #}
-}
-vim.o.statusline = table.concat(stl)
-
 vim.pack.add({
   "https://github.com/RRethy/base16-nvim",
   "https://github.com/savq/melange-nvim",
