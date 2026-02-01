18b6d6e93fd1a7a157d6c536457a96ae59c88dab
Ken Lee <ken.lee.dev@gmail.com>
2026-01-31 19:25:32 -0800

    Cleanup eslint config

--- .config/nvim/after/compiler/eslint.vim
+++ .config/nvim/after/compiler/eslint.vim
@@ -2,8 +2,8 @@ if exists(":CompilerSet") != 2
   command -nargs=* CompilerSet setlocal <args>
 endif
 
-CompilerSet errorformat=%f:\ line\ %l\\,\ col\ %c\\,\ %m,%-G%.%#
 CompilerSet makeprg=eslint_d\ --format\ compact
+CompilerSet errorformat=%f:\ line\ %l\\,\ col\ %c\\,\ %m,%-G%.%#
 
 " autocmd UserLint BufWritePost <buffer> silent lmake! <afile> | silent redraw!
 autocmd UserLint VimLeave * !eslint_d stop
--- .config/nvim/after/ftplugin/javascript/prg.lua
+++ .config/nvim/after/ftplugin/javascript/prg.lua
@@ -1,16 +1,13 @@
-local executable = vim.fn.executable
-local opt_local = vim.opt_local
-
 -- makeprg ---------------------------------------------------------------------
 
 -- eslint_d: use `:lmake %` to populate loclist.
-if executable("eslint_d") == 1 then
+if vim.fn.executable("eslint_d") == 1 then
   vim.cmd("compiler eslint")
 end
 
 -- formatprg -------------------------------------------------------------------
 
 -- prettier: use `gq` operator.
-if executable("prettier") == 1 then
-  opt_local.formatprg = "npx prettier --stdin-filepath % --trailing-comma all"
+if vim.fn.executable("prettier") == 1 then
+  vim.opt_local.formatprg = "npx prettier --stdin-filepath % --trailing-comma all"
 end
