fb79f13911a0c223355a910c51d5dfc862bf63c4
Ken Lee <ken.lee.dev@gmail.com>
2026-02-05 09:39:31 -0800

    Fix: duplicate shellcheck messages in loclist

--- .config/nvim/after/ftplugin/sh/prg.lua
+++ .config/nvim/after/ftplugin/sh/prg.lua
@@ -1,8 +1,20 @@
 -- makeprg ---------------------------------------------------------------------
 
+-- romainl: vim apparently ships with a "compiler" for shellcheck
+-- (so setting formatprg and errorformat in after/compiler/ only causes problems).
+-- https://www.reddit.com/r/vim/comments/pgjsqb/comment/hbffy2i/
+
 -- shellcheck: use `:lmake %` to populate loclist.
 if vim.fn.executable("shellcheck") == 1 then
   vim.cmd("compiler shellcheck")
+
+  local user_lint = vim.api.nvim_create_augroup("UserLint", { clear = true })
+  vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+    desc = "Run makeprg on buffer after save.",
+    pattern = "<buffer>",
+    group = user_lint,
+    command = "silent lmake! % | silent redraw!",
+  })
 end
 
 -- formatprg -------------------------------------------------------------------
@@ -13,7 +25,7 @@ if vim.fn.executable("shfmt") == 1 then
 
   -- local user_lint = vim.api.nvim_create_augroup("UserLint", { clear = true })
   -- vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  --   desc = "Run formatprg before save.",
+  --   desc = "Run formatprg on buffer before save.",
   --   pattern = "<buffer>",
   --   group = user_lint,
   --   command = "silent normal gggqG",
