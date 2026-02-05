9245d99a7f3714a8d1d0f8372efd6297ea797b6e
Ken Lee <ken.lee.dev@gmail.com>
2026-02-04 20:37:50 -0800

    LSP: add completion options

--- .config/nvim/plugin/lsp.lua
+++ .config/nvim/plugin/lsp.lua
@@ -1,13 +1,3 @@
--- New default mappings (LSP-related):
--- grn       = Normal mode -> vim.lsp.buf.rename()
--- grr       = Normal mode -> vim.lsp.buf.references()
--- gri       = Normal mode -> vim.lsp.buf.implementation()
--- gO        = Normal mode -> vim.lsp.buf.document_symbol() (this is analogous to the gO mappings in help buffers and :Man page buffers to show a "table of contents")
--- gra       = Normal / Visual modes. -> vim.lsp.buf.code_action()
--- K         = Normal mode -> vim.lsp.buf.hover()
--- CTRL-S    = Insert / Select modes. -> vim.lsp.buf.signature_help()
--- [d and ]d = Move between diagnostics in the current buffer ([D jumps to the first diagnostic, ]D jumps to the last)
-
 vim.diagnostic.config({
   virtual_text = { current_line = true },
 })
@@ -57,11 +47,22 @@ local function attached_clients()
   return language_servers
 end
 
+-- New default mappings (LSP-related):
+-- grn       = Normal mode -> vim.lsp.buf.rename()
+-- grr       = Normal mode -> vim.lsp.buf.references()
+-- gri       = Normal mode -> vim.lsp.buf.implementation()
+-- gO        = Normal mode -> vim.lsp.buf.document_symbol() (this is analogous to the gO mappings in help buffers and :Man page buffers to show a "table of contents")
+-- gra       = Normal / Visual modes. -> vim.lsp.buf.code_action()
+-- K         = Normal mode -> vim.lsp.buf.hover()
+-- CTRL-S    = Insert / Select modes. -> vim.lsp.buf.signature_help()
+-- [d and ]d = Move between diagnostics in the current buffer ([D jumps to the first diagnostic, ]D jumps to the last)
+
 -- basic LSP-based auto-completion via gpanders
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(event)
     local client = vim.lsp.get_client_by_id(event.data.client_id)
     if client:supports_method("textDocument/completion") then
+      vim.opt.completeopt = { "menu", "menuone", "noinsert", "fuzzy", "popup" }
       vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = true })
     end
 
