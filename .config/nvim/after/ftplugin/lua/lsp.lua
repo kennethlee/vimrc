062dd115155fe64b607c7ca4fdd735be4819a30b
Ken Lee <ken.lee.dev@gmail.com>
2026-02-10 19:42:19 -0800

    LSP: enable codelens for sumneko

--- .config/nvim/after/ftplugin/lua/lsp.lua
+++ .config/nvim/after/ftplugin/lua/lsp.lua
@@ -23,6 +23,7 @@ vim.lsp.start({
         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
         version = "LuaJIT",
       },
+      codeLens = { enable = true },
       diagnostics = {
         -- Get the language server to recognize the `vim` global
         globals = { "vim" },
