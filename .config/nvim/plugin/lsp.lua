31a28d1a354ebb3e9f9d772ccfa89657a221aa92
Ken Lee <ken.lee.dev@gmail.com>
2026-02-14 13:59:40 -0800

    LSP: add keymap (<Space>k)

--- .config/nvim/plugin/lsp.lua
+++ .config/nvim/plugin/lsp.lua
@@ -1,3 +1,13 @@
+-- New default mappings (LSP-related):
+-- grn       = Normal mode -> vim.lsp.buf.rename()
+-- grr       = Normal mode -> vim.lsp.buf.references()
+-- gri       = Normal mode -> vim.lsp.buf.implementation()
+-- gO        = Normal mode -> vim.lsp.buf.document_symbol() (this is analogous to the gO mappings in help buffers and :Man page buffers to show a "table of contents")
+-- gra       = Normal / Visual modes. -> vim.lsp.buf.code_action()
+-- K         = Normal mode -> vim.lsp.buf.hover()
+-- CTRL-S    = Insert / Select modes. -> vim.lsp.buf.signature_help()
+-- [d and ]d = Move between diagnostics in the current buffer ([D jumps to the first diagnostic, ]D jumps to the last)
+
 vim.diagnostic.config({
   virtual_text = { current_line = true },
 })
@@ -29,16 +39,6 @@ local function lsp_status()
   return "Attached LSP clients: [" .. table.concat(names, ", ") .. "]"
 end
 
--- New default mappings (LSP-related):
--- grn       = Normal mode -> vim.lsp.buf.rename()
--- grr       = Normal mode -> vim.lsp.buf.references()
--- gri       = Normal mode -> vim.lsp.buf.implementation()
--- gO        = Normal mode -> vim.lsp.buf.document_symbol() (this is analogous to the gO mappings in help buffers and :Man page buffers to show a "table of contents")
--- gra       = Normal / Visual modes. -> vim.lsp.buf.code_action()
--- K         = Normal mode -> vim.lsp.buf.hover()
--- CTRL-S    = Insert / Select modes. -> vim.lsp.buf.signature_help()
--- [d and ]d = Move between diagnostics in the current buffer ([D jumps to the first diagnostic, ]D jumps to the last)
-
 -- basic LSP-based auto-completion via gpanders
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(event)
@@ -47,7 +47,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
       vim.opt.completeopt = { "menu", "menuone", "noinsert", "fuzzy", "popup" }
       vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = true })
     end
-
     -- turn hints off to start; use `grh` to toggle on
     vim.lsp.inlay_hint.enable(false)
 
@@ -61,6 +60,16 @@ vim.api.nvim_create_autocmd("LspAttach", {
         vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({0}), {0})
       end
     )
+    vim.keymap.set("n", "<Space>k", function()
+      vim.diagnostic.config({ virtual_lines = { current_line = true }, virtual_text = false })
+      vim.api.nvim_create_autocmd("CursorMoved", {
+        group = vim.api.nvim_create_augroup("UserLint", { clear = true }),
+        callback = function()
+          vim.diagnostic.config({ virtual_lines = false, virtual_text = true })
+          return true
+        end,
+      })
+    end)
 
     print(lsp_status())
   end,
